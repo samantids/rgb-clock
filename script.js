ce9c1aeb84b03b8e0c8d8d299697fb63dec8f6e4
Samantha Schwartz <samanthadschwartz@gmail.com>
2017-05-09 21:38:32 -0400

    object clean up, change time display to divs

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 window.onload = function(){
 	var rgbButton = document.getElementById("get-rgb"),
 		rgbDiv = document.getElementById("rgb"),
-		/*red = 1,
-		green = 1,
-		blue = 1, 
-		redDescend = false,
-		greenDescend = false,
-		blueDescend = false,*/
+		clockHour = document.getElementById("clock-hour"),
+		clockMinute = document.getElementById("clock-minute"),
+		clockSecond = document.getElementById("clock-second"),
 		red = new Color (1, 2, 2),
 		green = new Color (2, 5, 5),
 		blue = new Color (1, 4, 4),
@@ -17,54 +14,34 @@ window.onload = function(){
 		this.descend = false;
 		this.increase = increase;
 		this.decrease = decrease;
+		this.colorChanger = colorChanger;
 	}
 
-	function colorChanger(color, colorbool, increase, decrease){
-		if (colorbool){
-			color+=increase;
+	function colorChanger(){
+		if (this.descend){
+			this.value-=this.decrease;
 		} else {
-			color-=decrease;
+			this.value+=this.increase;
 		}
-		if (color >= 255 || color <= 0){
-			colorbool = !colorbool;
+		if (this.value >= 255 || this.value <= 0){
+			this.descend = !(this.descend);
 		}
 	}
 
 	function myTimer() {
 	    var date = new Date(),
 	    	dateArr = date.toLocaleTimeString().split(":");
-	    document.getElementById("clock").innerHTML = dateArr[0] + "<br>" + dateArr[1] + "<br>" + dateArr[2];
 
+	    clockHour.textContent = dateArr[0];
+	    clockMinute.textContent = dateArr[1];
+	    clockSecond.textContent = dateArr[2];
+	    
+	    //document.getElementById("clock").innerHTML = dateArr[0] + "<br>" + dateArr[1] + "<br>" + dateArr[2];
 	    document.body.style.background = "rgb(" + red.value + "," + green.value + "," + blue.value + ")";
 
-	    //colorChanger(red, redDescend, 2, 2);
-
-	    if (red.descend){
-	    	red.value-=2;
-	    } else {
-	    	red.value+=2;
-	    }
-	    if (red.value >= 255 || red.value <= 0){
-	    	red.descend = !(red.descend);
-	    }
-
-	    if (green.descend){
-	    	green.value-=5;
-	    } else {
-	    	green.value+=5;
-	    }
-	    if (green.value >= 255 || green.value <= 0){
-	    	green.descend = !(green.descend);
-	    }
-
-	    if (blue.descend){
-	    	blue.value-=3;
-	    } else {
-	    	blue.value+=3;
-	    }
-	    if (blue.value >= 255 || blue.value <= 0){
-	    	blue.descend = !(blue.descend);
-	    }
+	    red.colorChanger();
+	    green.colorChanger();
+	    blue.colorChanger();
 	}
 
 
